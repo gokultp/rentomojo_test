acee7d640b9dd516ebcaf96d38f10f7a7e327d85
Gokul T P <tp.gokul@gmail.com>
2017-04-28 00:38:00 +0530

    testing queue

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var Queue       = require('./lib/jobqueue.js');
 
 
 queue       = new Queue(function (data, cb) {
-    console.log(data);
-    setTimeout(function () {
-        cb(data)
-        
-    }, 5000)
+    console.log('data',data);
+
+    test(data, function () {
+
+                            cb(data)
+
+    })
 }, 2);
 
 
@@ -14,7 +16,13 @@ a = [1,2,3,4,5,6,7,8,9];
 
 a.forEach(function(element) {
     queue.push(element, function (params) {
-        console.log('done', params);
+        console.log('done------', params);
         
     });
 });
+
+
+function test (data, done) {
+    console.log('test', data);
+    done(data)
+}
