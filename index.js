15c5243d7a15c80b462925d6cb3dc4ed0f6d7a62
Gokul T P <tp.gokul@gmail.com>
2017-04-28 03:49:03 +0530

    changed  queue

--- index.js
+++ index.js
@@ -1,28 +1,86 @@
-var Queue       = require('./lib/jobqueue.js');
+var JobQueue       = require('./lib/jobqueue.js');
+var request             = require('request');
+var parser              = require('./lib/parser')
+var fs                  = require('fs');
+var url                 = require('url');
+
+const CONCURRENCY       = 5;
+const FILE_NAME         = 'links.csv';
+var objUrlHashMap       = new Object();
+
+Queue       = new JobQueue(function (data, cb) {
+   makeRequest(data.href, function (err, body) {
+       if(err){
+        //    ignore errors
+           cb(null)
+       }
+       else {
+           parser.parseHtml(body, cb);
+       }
+   });
+}, CONCURRENCY);
 
 
-queue       = new Queue(function (data, cb) {
-    console.log('data',data);
 
-    test(data, function () {
-
-                            cb(data)
-
-    })
-}, 2);
-
-
-a = [1,2,3,4,5,6,7,8,9];
-
-a.forEach(function(element) {
-    queue.push(element, function (params) {
-        console.log('done------', params);
-        
+function makeRequest(url, cb) {
+    request(url, function (err, res, body) {
+        if(err){
+            cb(err);
+        }
+        else{
+            cb(null, body);
+        }
     });
-});
-
-
-function test (data, done) {
-    console.log('test', data);
-    done(data)
 }
+
+fs.appendFile(FILE_NAME, '"text","href"\r\n');
+
+
+function startScrape(data) {
+    console.log('--------------start')
+    Queue.push(data, function (err, res) {
+            console.log('--------------got')
+            if(res){
+                res.forEach(function(element, i) {
+
+                    element.href        = fixLink(element.href, data.href);
+                    element.text        = fixText(element.text);
+
+                    if(element.href && !objUrlHashMap[element.href]){
+                        objUrlHashMap[element.href]    = 1;
+                        fs.appendFile(FILE_NAME, '"'+ element.text+ '","'+ element.href+'"\r\n', function (err) {
+                            
+                        });
+                        startScrape(element)
+                    }
+                });
+            }
+            
+        });
+    
+    return
+}
+
+
+function fixText(txt){
+    // replace all new line characters
+    return txt.replace(/\n/g, ' ').replace(/\r/g, ' ');
+}
+
+function fixLink(elementUrl, parentPageUrl) {
+    // some urls will not have host for eg: for sin in it will be just '/signin'
+    // so here we have to take host from parent page and append with it
+    // here a library called url is used for that purpose.
+    
+    var currentUrl  = url.parse(elementUrl);
+    if(currentUrl.host){
+        return elementUrl;
+    }
+    var parentUrl   = url.parse(parentPageUrl);
+    return parentUrl.resolve(elementUrl);
+}
+
+startScrape({href: 'https://medium.com'});
+
+
+
